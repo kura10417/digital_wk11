4887f5a162bd44c6243827b11190f551adb597f4
kura10417 <kura10417@gmail.com>
2018-05-09 11:22:56 +0800

    Add files via upload

---
+++ wk11/wk11.ino
@@ -0,0 +1,18 @@
+
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(12, OUTPUT);
+  pinMode(13, OUTPUT);
+}
+
+
+void loop() {
+  digitalWrite(12, HIGH);   
+  delay(500);                      
+  digitalWrite(12, LOW);   
+  delay(500); 
+  digitalWrite(13, HIGH);  
+  delay(500);                  
+  digitalWrite(13, LOW);   
+  delay(500);                     
+}
